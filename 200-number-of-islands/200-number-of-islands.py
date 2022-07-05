791da3cc5c7f426c43ab9f2f9c2123052c149b47
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-07-05 16:46:38 -0400

    Time: 564 ms (27.56%), Space: 25.1 MB (10.68%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        visit = set()
+        count = 0
+        
+        def out_of_bounds(r, c):
+            return r in [-1, ROWS] or c in [-1, COLS]
+        
+        def dfs(r, c):
+            if out_of_bounds(r, c) or (r, c) in visit or grid[r][c] == "0":
+                return
+            visit.add((r, c))
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r, c) not in visit:
+                    count += 1
+                    dfs(r, c)
+                    
+        return count
