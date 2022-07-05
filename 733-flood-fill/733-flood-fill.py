395b49906344b012951d7121467615bd64056e27
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-07-05 13:06:49 -0400

    Time: 157 ms (9.81%), Space: 14.2 MB (13.95%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,22 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        ROWS, COLS = len(image), len(image[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        visit = set()
+        original_color = image[sr][sc]
+        
+        def out_of_bounds(r, c):
+            return r < 0 or c < 0 or r == ROWS or c == COLS
+        
+        def dfs(r, c):
+            if out_of_bounds(r, c) or (r, c) in visit:
+                return
+            
+            visit.add((r, c))
+            if image[r][c] == original_color:
+                image[r][c] = color
+                for dr, dc in directions:
+                    dfs(r + dr, c + dc)
+                
+        dfs(sr, sc)
+        return image
