38aa71ea35a77d9af1f6fbcde0d88094d1704908
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-07-07 23:42:25 -0400

    Time: 879 ms (54.21%), Space: 16 MB (15.34%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
+        memo = []
+        for r in range(N):
+            row = []
+            for c in range(N):
+                row.append(-1)
+            memo.append(row)
+        
+        def outOfBounds(r, c):
+            return r in [-1, N] or c in [-1, N]
+        
+        visit = set()
+        q = deque()
+        for r in range(N):
+            for c in range(N):
+                if grid[r][c]:
+                    q.append((r, c))
+                    visit.add((r, c))
+        
+        level = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                memo[r][c] = level
+                # print((r, c), " - ", str(memo))
+                for dr, dc in directions:
+                    newR, newC = r + dr, c + dc
+                    if not outOfBounds(newR, newC) and (newR, newC) not in visit and not grid[newR][newC]:
+                        q.append((newR, newC))
+                        visit.add((newR, newC))
+                    
+            level += 1
+            
+        maxDistance = -1
+        for r in range(N):
+            for c in range(N):
+                # print("Memo: ", (r, c), str(memo[r][c]))
+                if memo[r][c] > maxDistance:
+                    maxDistance = memo[r][c]
+                    
+        return maxDistance if maxDistance > 0 else -1
