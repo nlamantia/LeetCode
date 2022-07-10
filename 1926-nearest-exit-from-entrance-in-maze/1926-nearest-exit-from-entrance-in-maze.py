a137641a705ea29bfb2a8ff0dd57c8f641ec4ead
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-07-09 23:47:19 -0400

    Time: 1263 ms (41.68%), Space: 15.4 MB (42.99%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        ROWS, COLS = len(maze), len(maze[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        
+        def outOfBounds(r, c):
+            return r in [-1, ROWS] or c in [-1, COLS]
+        
+        def isExit(r, c):
+            return (r in [0, ROWS - 1] or c in [0, COLS - 1]) and maze[r][c] == "."
+        
+        q = deque([(entrance[0], entrance[1], 0)])
+        visit = set([(entrance[0], entrance[1])])
+        while q:
+            # print(str(q))
+            r, c, dist = q.popleft()
+            if isExit(r, c) and [r, c] != entrance:
+                return dist
+            
+            for dr, dc in directions:
+                newR, newC = r + dr, c + dc
+                if not outOfBounds(newR, newC) and (newR, newC) not in visit and maze[newR][newC] != "+":
+                    q.append((newR, newC, dist + 1))
+                    visit.add((newR, newC))
+            
+        return -1
+                    
