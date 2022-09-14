4e9fba58e3467630af0b5268024bcbfa20fec0ef
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-09-14 09:59:12 -0400

    Time: 21 ms (46.69%), Space: 43.1 MB (68.31%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,10 +1,14 @@
+// Complexity analysis - let m be the length of s1 and n be the length of s2
 class Solution {
+    // O(n * m) time | O(1)
     public boolean checkInclusion(String s1, String s2) {
+        // O(m) time
         int[] s1Counts = new int['z' - 'a' + 1];
         for (int i = 0; i < s1.length(); i++) {
             s1Counts[s1.charAt(i) - 'a']++;
         }
         
+        // O(n * m) time
         int start = 0, end = 0;
         int[] s2Counts = new int['z' - 'a' + 1];
         while (end < s2.length()) {
@@ -20,6 +24,7 @@ class Solution {
         return false;
     }
     
+    // O(m) time | O(1) space
     private boolean isPermutation(String s1, int[] s1Counts, int[] s2Counts) {
         for (int i = 0; i < s1.length(); i++) {
             int index = s1.charAt(i) - 'a';
