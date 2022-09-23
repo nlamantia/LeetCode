d67d6d5cedf1837b2d2bbca1e13750c16868c068
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-09-23 12:34:14 -0400

    Time: 170 ms (8.57%), Space: 211.2 MB (41.71%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.rb
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.rb
@@ -1,6 +1,9 @@
 # @param {Integer[][]} matrix
 # @param {Integer} target
 # @return {Boolean}
+#
+# O(m + n) time | O(1) space
+# where m is the number of rows and n is the number of columns in the matrix
 def search_matrix(matrix, target)
     row, col = [0, matrix[0].length - 1]
     while row < matrix.length && col >= 0
