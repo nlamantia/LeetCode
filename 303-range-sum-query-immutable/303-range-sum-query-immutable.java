9986a20969ac369709deca8e1650f3b6b24e545d
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-09-25 22:54:41 -0400

    Time: 23 ms (33.94%), Space: 49.2 MB (40.90%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.java
@@ -2,9 +2,8 @@ class NumArray {
     private int[] sums;
 
     public NumArray(int[] nums) {
-        sums = new int[nums.length];
-        sums[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
+        sums = nums;
+        for (int i = 1; i < sums.length; i++) {
             sums[i] = sums[i - 1] + nums[i];
         }
     }
