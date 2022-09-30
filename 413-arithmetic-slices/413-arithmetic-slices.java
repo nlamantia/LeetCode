db6e2cc050ca97ab5c06ac3ed1946cb23abd0388
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-09-30 12:01:27 -0400

    Time: 2 ms (15.29%), Space: 41.9 MB (47.55%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,15 +1,15 @@
 class Solution {
+    // O(n^2) time | O(n) space
     public int numberOfArithmeticSlices(int[] nums) {
         if (nums == null || nums.length <= 2) return 0;
         
-        int[] memo = new int[nums.length];
+        int currentNum = 0;
         for (int i = nums.length - 3; i >= 0; i--) {
             int diff = nums[i + 1] - nums[i];
             for (int j = i + 1; j < nums.length - 1 && nums[j + 1] - nums[j] == diff; j++) {
-                memo[i]++;
+                currentNum++;
             }
-            memo[i] += memo[i + 1];
         }
-        return memo[0];
+        return currentNum;
     }
 }
