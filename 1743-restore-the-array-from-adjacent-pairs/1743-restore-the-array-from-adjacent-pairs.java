700ff3bb51665110edb34e1a45c34cd813f030bd
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-09-29 22:40:33 -0400

    Time: 501 ms (5.02%), Space: 223.9 MB (5.02%) - LeetHub

---
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.java
@@ -0,0 +1,42 @@
+class Solution {
+    private Map<Integer, Set<Integer>> adj;
+    private int[] nums;
+    
+    // O(n) time | O(n) space
+    public int[] restoreArray(int[][] adjacentPairs) {
+        // build adjacency map
+        adj = new HashMap<>();
+        for (int[] pair : adjacentPairs) {
+            addAdjacentNumbers(pair[0], pair[1]);
+            addAdjacentNumbers(pair[1], pair[0]);
+        }
+        
+        // dfs from first node we find that has only 1 neighbor
+        nums = new int[adjacentPairs.length + 1];
+        for (Map.Entry<Integer, Set<Integer>> entry : adj.entrySet()) {
+            if (entry.getValue().size() == 1) {
+                dfs(entry.getKey(), 0, new HashSet<>());
+            }
+        }
+        return nums;
+    }
+    
+    private void dfs(int current, int nextIndex, Set<Integer> visited) {
+        if (visited.contains(current)) {
+            return;
+        }
+        
+        visited.add(current);
+        nums[nextIndex] = current;
+        
+        for (Integer neighbor : adj.get(current)) {
+            dfs(neighbor, nextIndex + 1, visited);
+        }
+    }
+    
+    private void addAdjacentNumbers(int src, int dest) {
+        Set<Integer> neighbors = adj.getOrDefault(src, new HashSet<>());
+        neighbors.add(dest);
+        adj.put(src, neighbors);
+    }
+}
