8599d219e3c809b921e518fbfedfa52424e3169e
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-10-18 22:54:12 -0400

    Time: 2 ms (90.02%), Space: 48 MB (64.80%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            result[i] = i % 2 == 0 ? result[i / 2] : result[i - 1] + 1;
+        }
+        return result;
+    }
+}
