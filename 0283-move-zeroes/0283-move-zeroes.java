c517b857fdf2e9bc9163a33a09e4d266cacae914
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-10-20 22:45:48 -0400

    Time: 63 ms (9.39%), Space: 54.7 MB (58.93%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int z = 0, nz = 0;
+        while (z < nums.length && nz < nums.length) {
+            // find next zero
+            while (z < nums.length && nums[z] != 0) {
+                z++;
+            }
+            
+            // find next non-zero
+            nz = z;
+            while (nz < nums.length && nums[nz] == 0) {
+                nz++;
+            }
+            
+            // swap the next zero with the next non-zero
+            if (z < nums.length && nz < nums.length) {
+                nums[z] = nums[nz];
+                nums[nz] = 0;
+            }
+            
+            z++;
+        }
+    }
+}
