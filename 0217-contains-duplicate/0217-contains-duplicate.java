e2678fb6430c6e51aa98211a48af348c0c97f0f0
Nick LaMantia <nicholas.lamantia@joinroot.com>
2022-10-29 21:46:21 -0400

    Time: 21 ms (51.89%), Space: 69.7 MB (41.96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    // O(n log n) time | O(1) space
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
