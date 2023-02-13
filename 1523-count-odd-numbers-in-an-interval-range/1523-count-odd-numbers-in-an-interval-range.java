a4a166003965649a9880e51dcdd388488878bc53
Nick LaMantia <nicholas.lamantia@joinroot.com>
2023-02-13 12:31:10 -0500

    Time: 0 ms (100.00%), Space: 39 MB (80.59%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        int odds = (high - low) / 2;
+        if (low % 2 != 0 || high % 2 != 0) odds++;
+        return odds;
+    }
+}
